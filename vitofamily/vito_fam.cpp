4e632736879b3210dd80219b87abae7bcb632438
khanhlearnin12 <hakhanh12.study@gmail.com>
2026-03-22 03:49:58 +0800

    new update

--- vitofamily/vito_fam.cpp
+++ vitofamily/vito_fam.cpp
@@ -1,28 +1,45 @@
-#include <iostream>
+#include <iostream> 
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <cmath>
 
 using namespace std;
-#define house_length 30000 
 
 int main(){
-    int number_case ;
-    cin >> number_case;
-    int result, save_result = 0;
-    //test thru each case 
-    for (int i = 0 ; i < number_case ; i++){ 
-        int house_number, house_num[house_length]; 
-        cin >> house_number;
-        //input data 
-        for (int i = 0 ; i < house_number; i++){
-            cin >> house_num[i];
+    int test_cases;
+    cin >> test_cases;
+    while(test_cases--){
+        int house;
+        cin >> house;
+        vector<int> house_cumulate;
+        for (int i = 0; i < house; i++){
+            int x;
+            cin >> x;
+            house_cumulate.push_back(x);
         }
-       //back - font = result and result + the previous result  
-        for (int i = 0 ; i < house_number; i++){
-            for (int j = i+1; j < house_number; j++) {
-                result = house_num[j] - house_num[i];
-                save_result += result; 
-            }
+        
+        sort(house_cumulate.begin(), house_cumulate.end());
+         
+        int median = house_cumulate[house / 2];
+
+        int result = 0 ;
+        for (int i = 0; i < house ; i++)
+        {
+            result += abs(house_cumulate[i] - median);
         }
-        cout << save_result << endl;
+        cout << result << endl;
     }
     return 0;
 }
+
+/*
+approach way 
+find the sorted way to relative house 
+so we should use abs(frist_house - median(the closest house))
+ideas : 
+    input cases
+    each case input house, number_of_houses_distence ;
+    we create the median things to find the closest house to go to 
+    for each house distence we going to sum up that.
+*/
